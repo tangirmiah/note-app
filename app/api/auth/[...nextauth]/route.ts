b49c288dc09f0b614bc13567cf0b73a53f048867
tangir <tangir.miah@gmail.com>
2023-06-02 22:12:33 +0100

    google provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 const handler = NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -7,10 +8,10 @@ const handler = NextAuth({
       clientId: process.env.GITHUB_ID || "",
       clientSecret: process.env.GITHUB_SECRET || "",
     }),
-    // GoogleProvider({
-    //   clientId: process.env.GOOGLE_ID || "",
-    //   clientSecret: process.env.GOOGLE_SECRET || "",
-    // }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID || "",
+      clientSecret: process.env.GOOGLE_SECRET || "",
+    }),
     // ...add more providers here
   ],
   secret: process.env.JWT_SECRET,
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,7 +5,7 @@ import { signOut, useSession } from "next-auth/react";
 export default function NavbarContainer() {
   const { data: session } = useSession();
   return (
-    <Navbar height={600} p="xs" width={{ base: 300 }}>
+    <Navbar p="xs" width={{ base: 300 }}>
       <Navbar.Section grow mt="md">
         <h1>main</h1>
       </Navbar.Section>
